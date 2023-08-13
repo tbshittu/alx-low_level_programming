30aa004b11e2a25074be7d6ff8e013b4a46a033a
tbshittu <tbshittu@gmail.com>
2023-08-13 15:41:11 -0700

    Do not use putchar more than twice

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,18 +10,13 @@
  */
 int main(void)
 {
-	putchar('0' + 0);
-	putchar('0' + 1);
-	putchar('0' + 2);
-	putchar('0' + 3);
-	putchar('0' + 4);
-	putchar('0' + 5);
-	putchar('0' + 6);
-	putchar('0' + 7);
-	putchar('0' + 8);
-	putchar('0' + 9);
+	char digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+		putchar(digit);
 
 	putchar('\n');
 
 	return (0);
 }
+
