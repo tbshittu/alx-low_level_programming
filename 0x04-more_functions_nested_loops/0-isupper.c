3cb2b8c42f672abe9615863e86f6fa312472d6fc
tbshittu <tbshittu@gmail.com>
2023-08-17 08:39:25 -0700

    0-isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - Check if a letter is uppercase
+ * @b: The number to be cyhecked
+ * Return: 1 for uppercase and 0 for anything else
+ */
+
+int _isupport(int x)
+
+{
+	if (b >= 65 && b <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
