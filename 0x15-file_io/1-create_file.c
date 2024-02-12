0357bf87e691a5b10fea65fdd55064df7563aeff
tbshittu <tbshittu@gmail.com>
2024-02-12 04:08:50 -0800

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	file_d = open(filename, 0_CREAT | O_WRONLY | O_TRUNC, 0600);
+	file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (file_d == -1)
 		return (-1);
