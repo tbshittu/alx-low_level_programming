61b48b7f54d3288b900d896d51850f5d132bada2
tbshittu <tbshittu@gmail.com>
2024-02-12 04:06:57 -0800

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 
-	for (nletters = 0; text_content(nletters); nletters++)
+	for (nletters = 0; text_content[nletters]; nletters++)
 		;
 
 	rwr = write(file_d, text_content, nletters);
